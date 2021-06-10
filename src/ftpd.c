8d9c22bad4bf6f03d102e933946bee959204cda8
zhen <zhendng@gmail.com>
2021-06-10 17:27:42 +0800

    remove static

--- src/ftpd.c
+++ src/ftpd.c
@@ -5551,7 +5551,7 @@ static struct passwd *fakegetpwnam(const char * const name)
 }
 #endif
 
-static int pureftpd_start(int argc, char *argv[], const char *home_directory_)
+int pureftpd_start(int argc, char *argv[], const char *home_directory_)
 {
     char* inputFile = argv[argc - 1];
     printf("input path %s", inputFile);
