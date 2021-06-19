079e0e4a774f2a2e63f88687e0e68b392934b596
Zhen <zhendng@gmail.com>
2021-06-19 14:13:20 +0100

    init data

---
+++ init.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+HOME=$1
+groupadd ftpgroup
+useradd -g ftpgroup -d /dev/null -s /etc ftpuser
+/bin/echo -e "a\na" | ./src/pure-pw useradd a -u ftpuser -d /home -f /pureftpd.passwd
+/bin/echo -e "b\nb" | ./src/pure-pw useradd b -u ftpuser -d /home -f /pureftpd.passwd
+/bin/echo -e "c\nc" | ./src/pure-pw useradd c -u ftpuser -d /home -f /pureftpd.passwd
+/bin/echo -e "d\nd" | ./src/pure-pw useradd d -u ftpuser -d /home -f /pureftpd.passwd
+/bin/echo -e "z\nz" | ./src/pure-pw useradd z -u ftpuser -d /home -f /pureftpd.passwd
+
+# Generate the authentication database
+./src/pure-pw mkdb /pureftpd.pdb -f /pureftpd.passwd
+
+# Prepare the ftp root directory
+mkdir $HOME/a
+mkdir $HOME/b
+mkdir $HOME/c
+mkdir $HOME/c/z
+mkdir $HOME/d
+mkdir $HOME/e
+mkdir $HOME/test
+dd if=/dev/urandom of=$HOME/m count=3 bs=3
+dd if=/dev/urandom of=$HOME/a/e count=3 bs=3
+dd if=/dev/urandom of=$HOME/b/q count=3 bs=3
+dd if=/dev/urandom of=$HOME/c/t count=3 bs=3
+dd if=/dev/urandom of=$HOME/c/z/t count=3 bs=3
+dd if=/dev/urandom of=$HOME/a/e1 count=3 bs=3
+dd if=/dev/urandom of=$HOME/test.txt count=3 bs=3
+dd if=/dev/urandom of=$HOME/test/test.txt count=3 bs=3
